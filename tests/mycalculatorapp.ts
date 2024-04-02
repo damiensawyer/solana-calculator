86688bbd01bf38c751121517d0c7c5dc1c80bde0
Damien Sawyer <damiensawyer@gmail.com>
2024-04-03 06:55:55 +1100

    progress

--- tests/mycalculatorapp.ts
+++ tests/mycalculatorapp.ts
@@ -1,16 +1,37 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Mycalculatorapp } from "../target/types/mycalculatorapp";
+import * as anchor from '@coral-xyz/anchor'
+
+// const anchor2 = require("@coral-xyz/anchor");
+// const assert = require("assert");
+const { SystemProgram } = anchor.web3;
 
 describe("mycalculatorapp", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.local();
+  
+  anchor.setProvider(provider);
+  const calculator = anchor.web3.Keypair.generate();
+  const program = anchor.workspace.Mycalculatorapp;
 
-  const program = anchor.workspace.Mycalculatorapp as Program<Mycalculatorapp>;
-
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Creates a calculator", async () => {
+    await program.rpc.create("hello Solana", {
+      accounts: {
+        calculator: calculator.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+    });
   });
 });
+
+// describe("mycalculatorapp", () => {
+//   // Configure the client to use the local cluster.
+//   anchor.setProvider(anchor.AnchorProvider.env());
+
+//   const program = anchor.workspace.Mycalculatorapp as Program<Mycalculatorapp>;
+
+//   it("Is initialized!", async () => {
+//     // // Add your test here.
+//     // const tx = await program.methods.initialize().rpc();
+//     // console.log("Your transaction signature", tx);
+//     assert(false)
+//   });
+// });
